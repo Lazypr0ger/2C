e589cec81dab82fadf31f13f2e7de6ae1c129185
Lazypr0ger <dmitrievandreywork@mail.ru>
2026-01-16 15:05:38 +0400

    startMiddle settings

---
+++ MiddleWareOcelot/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace MiddleWareOcelot
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BuildWebHost(args).Run();
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("configuration.json", optional: false, reloadOnChange: true);
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ MiddleWareOcelot/Startup.cs
@@ -0,0 +1,36 @@
+﻿using MMLib.SwaggerForOcelot.DependencyInjection;
+using MMLib.SwaggerForOcelot.Middleware;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace MiddleWareOcelot
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ВАЖНО: SwaggerForOcelot
+            services.AddSwaggerForOcelot(Configuration);
+
+            // Ocelot
+            services.AddOcelot(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            // UI будет доступен по /swagger
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
